3d05c58b2aa1e21a99cba58ab4836be6be7ed586
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-01-24 20:21:43 +0200

    Create main features

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import  "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="title">
+        Todo List
+        </h1>
+    </header>
+  )
+}
+
+export default Header
